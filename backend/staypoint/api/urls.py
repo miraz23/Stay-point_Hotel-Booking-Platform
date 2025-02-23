0b31f37c760dcd7dd5808c992ebae3fe960d6f5a
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-02-23 22:54:50 +0600

    Jwt initial setup

--- backend/staypoint/api/urls.py
+++ backend/staypoint/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from api import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('', views.getRoutes,name="getRoutes"),
     path('hotels/', views.getHotels,name="getHotels"),
     path('hotels/<int:pk>/', views.getHotel,name="getHotel"),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
