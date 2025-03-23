a65c062e15eb6224393f815eac1088e723d5ed7d
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-03-24 01:43:25 +0600

    Formated checkin checkout time

--- frontend/staypoint/src/Components/HotelDashboard.jsx
+++ frontend/staypoint/src/Components/HotelDashboard.jsx
@@ -5,7 +5,7 @@ import { listHotelDetails } from '../actions/hotelActions';
 import Loader from './Loader';
 import Message from './Message';
 import { MapPinIcon, StarIcon } from '@heroicons/react/24/solid';
-import { IconEdit, IconClock, IconBellQuestion, IconWifi, IconSwimming, IconAirConditioning, IconCar, IconBarbell, IconToolsKitchen3  } from '@tabler/icons-react';
+import { IconEdit, IconCircleDashedPlus, IconClock, IconBellQuestion, IconWifi, IconSwimming, IconAirConditioning, IconCar, IconBarbell, IconToolsKitchen3  } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
 
 const HotelDashboard = () => {
@@ -31,79 +31,94 @@ const HotelDashboard = () => {
       "Restaurant": <IconToolsKitchen3 />,
   };
 
-    return (
-        <div className="text-center my-30 mx-10">
-            {loading ? (
-                <Loader />
-            ) : error ? (
-                <Message variant='danger'>{error}</Message>
-            ) : (
-              <div>
-                <div className='w-full flex gap-x-10'>
-                    <div className='w-1/2 aspect-[3/2]'>
-                        {hotel.image ? (
-                            <img src={`http://127.0.0.1:8000/${hotel.image}`} alt={hotel.name} className="w-full h-full object-cover rounded-lg" />
-                        ) : (
-                            <img src="/default-hotel.jpg" alt={hotel.name} className="w-full h-full object-cover rounded-lg" />
-                        )}
-                    </div>
-                    <div className='w-1/2 text-left'>
-                      <div className='flex items-center justify-between'>
-                        <div className='w-full'>
-                            <div className='flex justify-between'>
+  const formatTime = (timeString) => {
+    const [hours, minutes] = timeString.split(":");
+    const date = new Date();
+    date.setHours(hours, minutes);
+    return date.toLocaleString("en-US", { hour: "numeric", minute: "2-digit", hour12: true });
+  };
+
+
+  return (
+      <div className="text-center my-30 mx-10">
+          {loading ? (
+              <Loader />
+          ) : error ? (
+              <Message variant='danger'>{error}</Message>
+          ) : (
+            <div>
+              <div className='w-full flex gap-x-10'>
+                  <div className='w-1/2 aspect-[3/2]'>
+                      {hotel.image ? (
+                          <img src={`http://127.0.0.1:8000/${hotel.image}`} alt={hotel.name} className="w-full h-full object-cover rounded-lg" />
+                      ) : (
+                          <img src="/default-hotel.jpg" alt={hotel.name} className="w-full h-full object-cover rounded-lg" />
+                      )}
+                  </div>
+                  <div className='w-1/2 text-left'>
+                    <div className='flex items-center justify-between pb-2'>
+                      <div className='w-full'>
+                          <div className='flex justify-between'>
+                            <div className='flex items-center'>
                               <h1 className='text-4xl font-semibold'>{hotel.name}</h1>
-                              <div className="flex items-center">
-                                <StarIcon className="h-5 w-5 text-yellow-500 mr-2"/>
-                                <span className="text-gray-600 text-2xl font-semibold">{hotel.rating}</span>
-                              </div>
+                              <Link>
+                                <button className="text-cyan-500 cursor-pointer hover:text-cyan-600 transition-colors p-2 rounded-md relative" title="Update Hotel">
+                                  <IconEdit size={28} />
+                                </button>
+                              </Link>
                             </div>
-                            <div className="flex items-center py-3">
-                              <MapPinIcon className="h-6 w-6 text-blue-500 mr-2" />
-                              <span className="text-gray-600">{hotel.location}</span>
-                            </div>
-                        </div>
-                      </div>
-                      <div className='border-1 border-gray-300 rounded-xl p-5'>
-                        <div className='pb-5 border-b-1 border-gray-300'>
-                          <h1 className='text-gray-700 text-xl mb-2 font-semibold'>About</h1>
-                          <p className='text-[16px]'>{hotel.description}</p>
-                        </div>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                          <div className="flex items-center pt-5">
-                            <IconClock className='mr-2' />
-                            <div>
-                              <p className="text-md text-muted-foreground">Check-in</p>
-                              <p className="text-md font-medium">{hotel.check_in_time}</p>
+                            <div className="flex items-center">
+                              <StarIcon className="h-5 w-5 text-yellow-500 mr-2"/>
+                              <span className="text-gray-600 text-2xl font-semibold">{hotel.rating}</span>
                             </div>
                           </div>
-                          <div className="flex items-center pt-5">
-                            <IconClock className='mr-2' />
-                            <div>
-                              <p className="text-md text-muted-foreground">Check-out</p>
-                              <p className="text-md font-medium">{hotel.check_out_time}</p>
-                            </div>
+                          <div className="flex items-center py-3">
+                            <MapPinIcon className="h-6 w-6 text-blue-500 mr-2" />
+                            <span className="text-gray-600">{hotel.location}</span>
+                          </div>
+                      </div>
+                    </div>
+                    <div className='border-1 border-gray-300 rounded-xl p-5'>
+                      <div className='pb-5 border-b-1 border-gray-300'>
+                        <h1 className='text-gray-700 text-xl mb-2 font-semibold'>About</h1>
+                        <p className='text-[16px]'>{hotel.description}</p>
+                      </div>
+                      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div className="flex items-center pt-5">
+                          <IconClock className='mr-2' />
+                          <div>
+                            <p className="text-md text-muted-foreground">Check-in</p>
+                            <p className="text-md font-medium">{formatTime(hotel.check_in_time)}</p>
+                          </div>
+                        </div>
+                        <div className="flex items-center pt-5">
+                          <IconClock className='mr-2' />
+                          <div>
+                            <p className="text-md text-muted-foreground">Check-out</p>
+                            <p className="text-md font-medium">{formatTime(hotel.check_out_time)}</p>
                           </div>
                         </div>
                       </div>
                     </div>
-                </div>
-                <div className='mt-10 border-1 border-gray-300 rounded-xl p-5'>
-                  <div>
-                    <h1 className='text-gray-700 text-left text-xl pb-5 font-semibold'>Popular Amenities</h1>
-                    <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
-                      {hotel.amenities && hotel.amenities.length > 0 && hotel.amenities.map((amenity, index) => (
-                        <div key={index} className='flex items-center'>
-                          <span className='bg-slate-200 p-2 rounded-full mr-2'>{amenityIcons[amenity]}</span>
-                          <span className='text-gray-700'>{amenity}</span>
-                        </div>
-                      ))}
-                    </div>
+                  </div>
+              </div>
+              <div className='mt-10 border-1 border-gray-300 rounded-xl p-5'>
+                <div>
+                  <h1 className='text-gray-700 text-left text-xl pb-5 font-semibold'>Popular Amenities</h1>
+                  <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
+                    {hotel.amenities && hotel.amenities.length > 0 && hotel.amenities.map((amenity, index) => (
+                      <div key={index} className='flex items-center'>
+                        <span className='bg-slate-200 p-2 rounded-full mr-2'>{amenityIcons[amenity]}</span>
+                        <span className='text-gray-700'>{amenity}</span>
+                      </div>
+                    ))}
                   </div>
                 </div>
               </div>
-            )}
-        </div>
-    );
+            </div>
+          )}
+      </div>
+  );
 };
 
 export default HotelDashboard;
